819971e5e6fc3fd265402bf4077e53c5c8419b41
Masa <masa@MasanoMacBook-Air.local>
2019-04-09 13:28:50 +0900

    add function from parents theme

--- functions/init.php
+++ functions/init.php
@@ -20,3 +20,15 @@ function remove_parent_theme_actions() {
   remove_action('wp_enqueue_scripts', 'st_enqueue_scripts', 10);
 }
 add_action('init','remove_parent_theme_actions');
+
+// カスタムメニュー
+// add_action( 'init', 'my_custom_menus' );
+// function my_custom_menus() {
+//     register_nav_menus(
+//         array(
+//             'primary-menu' => __( 'ヘッダー用メニュー', 'default' ),
+//             'secondary-menu' => __( 'フッター用メニュー', 'default' ),
+//             'smartphone-menu' => __( 'スマートフォン用メニュー', 'default' )
+//         )
+//     );
+// }
